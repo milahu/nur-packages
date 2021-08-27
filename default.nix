495ba3db066a885b7dd587786d957304f3110704
milahu <milahu@gmail.com>
2021-08-27 20:17:16 +0200

    aether: rename to aether-server, link in /default.nix

--- default.nix
+++ default.nix
@@ -18,6 +18,8 @@
 
   ricochet-refresh = pkgs.libsForQt5.callPackage ./pkgs/ricochet-refresh/default.nix { };
 
+  aether-server = pkgs.libsForQt5.callPackage ./pkgs/aether-server/default.nix { };
+
   # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...

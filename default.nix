3a0400b06cb403bada59e1400bc0e6c81b3b6e8b
Milan Hauth <milahu@gmail.com>
2022-03-03 20:13:46 +0100

    add gaupol

--- default.nix
+++ default.nix
@@ -42,6 +42,8 @@
 
   srtgen = pkgs.callPackage ./pkgs/srtgen { };
 
+  gaupol = pkgs.callPackage ./pkgs/gaupol/gaupol.nix { };
+
   # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...

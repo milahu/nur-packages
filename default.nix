3424a9ff0b7a614846820e0baea257a9431658df
Milan Hauth <milahu@gmail.com>
2022-03-08 01:35:12 +0100

    add autosub-by-abhirooptalasila

--- default.nix
+++ default.nix
@@ -44,6 +44,8 @@
 
   gaupol = pkgs.callPackage ./pkgs/gaupol/gaupol.nix { };
 
+  autosub-by-abhirooptalasila = pkgs.callPackage ./pkgs/autosub-by-abhirooptalasila { };
+
   # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...

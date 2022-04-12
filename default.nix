3a47211d51035c10d55b6f02981257077f7a54dc
Milan Hauth <milahu@gmail.com>
2022-04-12 16:30:51 +0200

    rename default.nix to autosub.nix

--- default.nix
+++ default.nix
@@ -56,7 +56,7 @@
 
   gaupol = pkgs.callPackage ./pkgs/gaupol/gaupol.nix { };
 
-  autosub-by-abhirooptalasila = pkgs.callPackage ./pkgs/autosub-by-abhirooptalasila { };
+  autosub-by-abhirooptalasila = pkgs.callPackage ./pkgs/autosub-by-abhirooptalasila/autosub.nix { };
 
   # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };

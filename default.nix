476ada7cee0b0ae79c0bb59f14941a2da75671e0
Milan Hauth <milahu@gmail.com>
2022-06-27 15:43:39 +0200

    aegisub: init at unstable-2022-06-08

--- default.nix
+++ default.nix
@@ -66,6 +66,8 @@
 
   tg-archive = pkgs.callPackage ./pkgs/tg-archive/tg-archive.nix { };
 
+  aegisub = pkgs.libsForQt5.callPackage ./pkgs/aegisub/aegisub.nix { };
+
   # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...

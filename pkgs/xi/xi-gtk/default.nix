c01ecd50c15b8cb9e5b2b98a8edade74d609a815
Milan Hauth <milahu@gmail.com>
2022-10-04 16:38:06 +0200

    xi-gtk: 2018-12-28 -> 2020-03-29

--- pkgs/xi/xi-gtk/default.nix
+++ pkgs/xi/xi-gtk/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchFromGitHub, meson, ninja, pkgconfig, gtk3, json-glib, vala, wrapGAppsHook, wrapXiFrontendHook }:
+{ lib, stdenv, fetchFromGitHub, meson, ninja, pkgconfig, gtk3, json-glib, vala, wrapGAppsHook, wrapXiFrontendHook }:
 
 stdenv.mkDerivation rec {
   name = "xi-gtk-${version}";
-  version = "2018-12-28";
+  version = "2020-03-29";
   
   src = fetchFromGitHub {
     owner = "eyelash";
     repo = "xi-gtk";
-    rev = "58918a268e82019f9265bfbea29f07e843bf980b";
-    sha256 = "0dvhj4yfkmsks5k3lvrzian2a3xv8ci73d3j8iiy78clm6h0sl4a";
+    rev = "753b5e083748e3bcebb199efce1ad89197b757e7";
+    sha256 = "sha256-OroX26ZbbDXWs6zjiltWf0DyIdrAO7sArPDESdV0EbE=";
   };
 
   nativeBuildInputs = [ meson ninja pkgconfig ];
@@ -21,11 +21,11 @@ stdenv.mkDerivation rec {
 
   postInstall = "wrapXiFrontend $out/bin/*";
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "A GTK+ front-end for the Xi editor";
     homepage = https://github.com/eyelash/xi-gtk;
     license = licenses.asl20;
-    maintainers = with maintainers; [ dtzWill ];
+    #maintainers = with maintainers; [ dtzWill ];
     platforms = platforms.all;
   };
 }

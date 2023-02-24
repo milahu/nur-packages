8a3e891f72d55488c31ca2e91026da7090ed3526
milahu <milahu@gmail.com>
2023-02-24 16:14:13 +0100

    add

---
+++ eval-test.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+nix-env -f . -qa \* --meta --xml \
+  --allowed-uris https://static.rust-lang.org \
+  --option restrict-eval true \
+  --option allow-import-from-derivation true \
+  --drv-path --show-trace \
+  -I nixpkgs=$(nix-instantiate --find-file nixpkgs) \
+  -I ./ \
+>/dev/null

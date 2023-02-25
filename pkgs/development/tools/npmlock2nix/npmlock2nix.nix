2d7260ca2b6003169b991edf90a177648264dcd8
milahu <milahu@gmail.com>
2023-02-25 10:15:12 +0100

    npmlock2nix: fix

---
+++ pkgs/development/tools/npmlock2nix/npmlock2nix.nix
@@ -0,0 +1,9 @@
+{ fetchFromGitHub }:
+
+fetchFromGitHub {
+  # https://github.com/nix-community/npmlock2nix
+  owner = "nix-community";
+  repo = "npmlock2nix";
+  rev = "eeed152290ec2425f96c5e74e469c40b621e1468";
+  sha256 = "sha256-HME6rnysvCwUVtH+BDWDGahmweMaLgD2wqHeRuGp6QI=";
+}

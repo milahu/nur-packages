392c0761b38820d20a174d539dd0b9252bef456f
Milan Hauth <milahu@gmail.com>
2023-04-09 13:02:46 +0200

    ffsubsync: fix webrtcvad

--- pkgs/applications/video/ffsubsync/ffsubsync.nix
+++ pkgs/applications/video/ffsubsync/ffsubsync.nix
@@ -37,7 +37,16 @@ python3.pkgs.buildPythonApplication rec {
     srt
     tqdm
     typing-extensions
-    webrtcvad
+    # webrtcvad is broken on runtime:
+    # ModuleNotFoundError: No module named 'pkg_resources'
+    # fix: add setuptools
+    # https://github.com/wiseman/py-webrtcvad/pull/87
+    # TODO fix: nixpkgs/pkgs/development/python-modules/webrtcvad/default.nix
+    (webrtcvad.overrideAttrs (oldAttrs: {
+      propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or []) ++ (with python3.pkgs; [
+        setuptools
+      ]);
+    }))
     #webrtcvad-wheels # binary release
   ];
 

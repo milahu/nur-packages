c96bf8e573e84145dc45d3747dd7d5fed85dc278
Milan Hauth <milahu@gmail.com>
2023-08-01 18:38:35 +0200

    cargo2nix: init at 0.11.0

--- default.nix
+++ default.nix
@@ -316,6 +316,8 @@ pkgs.lib.makeScope pkgs.newScope (self: let inherit (self) callPackage; in rec {
 
   radicle-bin = callPackage ./pkgs/applications/version-management/radicle/radicle-bin.nix { };
 
+  cargo2nix = callPackage ./pkgs/development/tools/rust/cargo2nix/cargo2nix.nix { };
+
 }
 
 # based on https://github.com/dtzWill/nur-packages

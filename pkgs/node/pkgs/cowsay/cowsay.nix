6380078e5e09cb66b2dc8c3405e792f64aa9ffd3
Milan Hauth <milahu@gmail.com>
2023-08-14 14:25:13 +0200

    nodePackages.cowsay: cleanup

--- pkgs/node/pkgs/cowsay/cowsay.nix
+++ pkgs/node/pkgs/cowsay/cowsay.nix
@@ -1,24 +1,9 @@
-/*
-fixme? can we just use old lockfiles?
-
-npm WARN old lockfile The package-lock.json file was created with an old version of npm,
-npm WARN old lockfile so supplemental metadata must be fetched from the registry.
-npm WARN old lockfile This is a one-time fix-up, please be patient...
-*/
-
 { lib
-, pkgs
 , fetchFromGitHub
-, callPackage
-, python3
-, git
 , npmlock2nix
 }:
 
 npmlock2nix.build rec {
-#npmlock2nix.v2.build rec {
-#npmlock2nix.granular-caching.build rec {
-#npmlock2nix.v1.build rec { # ok
 
   pname = "cowsay";
   version = "1.5.0";
@@ -30,19 +15,8 @@ npmlock2nix.build rec {
     hash = "sha256-TZ3EQGzVptNqK3cNrkLnyP1FzBd81XaszVucEnmBy4Y=";
   };
 
-  buildCommands = [
-    ''
-      set -x
-      #export HOME=$TMP
-      #npm run build --verbose --debug
-      npm run prepare --verbose --debug
-      #npm run build-desktop --verbose --debug
-      set +x
-    ''
-  ];
+  buildCommands = [ "npm run prepare" ];
 
-  # FIXME
-  #  mv -v $out/bin/redis-commander.js $out/bin/redis-commander
   installPhase = ''
     mkdir -p $out/opt
     cp -r . $out/opt/cowsay
@@ -50,30 +24,10 @@ npmlock2nix.build rec {
     ln -sr $out/opt/cowsay/cli.js $out/bin/cowsay
   '';
 
-  #node_modules_mode = "copy";
-
   node_modules_attrs = {
-
     symlinkNodeModules = true;
-
-    # force rebuild of node_packages
-    #  set -x
-    /*
-    preBuild = ''
-      echo "node_modules preBuild"
-      echo force rebuild...
-      echo force rebuild...
-    '';
-    */
-
     packageJson = ./package.json;
     packageLockJson = ./package-lock.json;
-    unpackNodePackages = true; # TODO implement
-    buildInputs = [
-    ];
-    githubSourceHashMap = {
-      #dotcs.license-ls."746b0835939930871332a501cd18f362245143c5" = "sha256-kr/Pawz66e9ljFlKO2+PTA677cv7JSoJG4ntxG8qSII=";
-    };
   };
 
   meta = with lib; {

26c7e51d88bd37030b298587f42a5720801c7d2d
Milan Hauth <milahu@gmail.com>
2023-08-25 16:09:05 +0200

    pdfium-bin: init at 5961

--- default.nix
+++ default.nix
@@ -522,6 +522,8 @@ pkgs.lib.makeScope pkgs.newScope (self: let inherit (self) callPackage; in rec {
   # https://github.com/NixOS/nixpkgs/commits/master/pkgs/tools/security/tor/torsocks.nix
   torsocks = callPackage ./pkgs/tools/security/tor/torsocks.nix { };
 
+  pdfium-bin = callPackage ./pkgs/development/libraries/pdfium-bin/pdfium-bin.nix { };
+
   gclient2nix = callPackage ./pkgs/build-support/gclient2nix/gclient2nix.nix { };
 
   gclient = callPackage ./pkgs/build-support/gclient/gclient.nix { };

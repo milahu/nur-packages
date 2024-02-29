88eae5821991017e75bc13f7c863cd7b12424b57
Milan Hauth <milahu@gmail.com>
2024-02-29 19:34:52 +0100

    python3.pkgs.stream-zip: init at 0.0.71

--- default.nix
+++ default.nix
@@ -342,6 +342,8 @@ pkgs.lib.makeScope pkgs.newScope (self: let inherit (self) callPackage; in rec {
 
       tree-sitter-languages = callPackage ./pkgs/python3/pkgs/tree-sitter-languages/tree-sitter-languages.nix { };
 
+      stream-zip = callPackage ./pkgs/python3/pkgs/stream-zip/stream-zip.nix { };
+
     #}))); # python3.pkgs
 
   #}))); # python3

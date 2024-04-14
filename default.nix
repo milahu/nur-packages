9906258fe758adc2e7066dea4038d2e186fc9b7b
Milan Hauth <milahu@gmail.com>
2024-04-14 14:07:36 +0200

    stp: init at unstable-2023-12-13

--- default.nix
+++ default.nix
@@ -737,6 +737,8 @@ pkgs.lib.makeScope pkgs.newScope (self: let inherit (self) callPackage; in rec {
 
   writable-nix-store = callPackage ./pkgs/development/tools/misc/writable-nix-store { };
 
+  stp = callPackage ./pkgs/applications/science/logic/stp { };
+
 }
 
 # based on https://github.com/dtzWill/nur-packages

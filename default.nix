d65d033c0b721712a95d9258a98ff1cb3e936f1a
Milan Hauth <milahu@gmail.com>
2024-04-15 18:34:57 +0200

    klee: remove

--- default.nix
+++ default.nix
@@ -739,8 +739,6 @@ pkgs.lib.makeScope pkgs.newScope (self: let inherit (self) callPackage; in rec {
 
   stp = callPackage ./pkgs/applications/science/logic/stp { };
 
-  klee = callPackage ./pkgs/applications/science/logic/klee { };
-
 }
 
 # based on https://github.com/dtzWill/nur-packages

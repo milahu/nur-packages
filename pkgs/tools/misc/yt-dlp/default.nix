54413de85f2d136c41b421f0a8db3081deb0ef4e
Milan Hauth <milahu@gmail.com>
2024-05-27 11:41:17 +0200

    yt-dlp: 2024.4.9 -> 2024.05.26

--- pkgs/tools/misc/yt-dlp/default.nix
+++ pkgs/tools/misc/yt-dlp/default.nix
@@ -1,6 +1,6 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , brotli
 , hatchling
 , certifi
@@ -25,13 +25,14 @@ buildPythonPackage rec {
   # The websites yt-dlp deals with are a very moving target. That means that
   # downloads break constantly. Because of that, updates should always be backported
   # to the latest stable release.
-  version = "2024.4.9";
+  version = "2024.05.26";
   pyproject = true;
 
-  src = fetchPypi {
-    inherit version;
-    pname = "yt_dlp";
-    hash = "sha256-fukFcrTTE7WCuZyJ5OzPd5tX/1TtwzGHPGs/unf6qLA=";
+  src = fetchFromGitHub {
+    owner = "yt-dlp";
+    repo = "yt-dlp";
+    rev = version;
+    hash = "sha256-hGmxuOZSEzGG+7ejwiSqLuzYmsbcK9aaoigiz1cVV/Y=";
   };
 
   build-system = [

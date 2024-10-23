96e66f8040143c73499f3e2287375b734cbaf7bc
Milan Hauth <milahu@gmail.com>
2024-10-23 16:14:03 +0200

    gns3-gui: unpin python3Packages
    revert d203ca98569fca1f9a0641a30cd9841b3b646c0a
    from https://github.com/NixOS/nixpkgs/pull/326646
    which created derivations with 2 python versions (3.11 + 3.12)
    which breaks the import of rpds:
    ModuleNotFoundError: No module named 'rpds.rpds'
    https://github.com/python-jsonschema/jsonschema/issues/1175

--- pkgs/applications/networking/gns3/default.nix
+++ pkgs/applications/networking/gns3/default.nix
@@ -1,12 +1,10 @@
 { callPackage
 , libsForQt5
-, python311Packages
 }:
 
 let
   mkGui = args: callPackage (import ./gui.nix (args)) {
     inherit (libsForQt5) wrapQtAppsHook;
-    python3Packages = python311Packages;
   };
 
   mkServer = args: callPackage (import ./server.nix (args)) { };

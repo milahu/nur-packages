b8c6a6a2745f0464cf8b14136ce64b2605cd774b
Milan Hauth <milahu@gmail.com>
2025-04-15 09:50:12 +0000

    libtorrent-rasterbar: use fetchurl

--- pkgs/development/libraries/libtorrent-rasterbar/default.nix
+++ pkgs/development/libraries/libtorrent-rasterbar/default.nix
@@ -1,7 +1,9 @@
 {
   lib,
   stdenv,
+  fetchurl,
   fetchFromGitHub,
+  unzip,
   cmake,
   zlib,
   boost,
@@ -21,11 +23,20 @@ let
   };
 
 in
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "libtorrent-rasterbar";
   inherit version;
 
-  src = fetchFromGitHub {
+  src =
+  # use fetchurl
+  # https://github.com/DavHau/nix-portable/issues/148
+  if true then
+  fetchurl {
+    url = "https://github.com/milahu/libtorrent/archive/377e4f5e482b7fe9d21ec853a9bfd2be3d29bb5c.zip";
+    hash = "sha256-A2YSHVO3ASzwHVDcpumu+re7WxyvsiixnY2R0m/uPAE=";
+  }
+  else
+  fetchFromGitHub {
 /*
     owner = "arvidn";
     repo = "libtorrent";
@@ -39,7 +50,40 @@ stdenv.mkDerivation {
     fetchSubmodules = true;
   };
 
-  nativeBuildInputs = [ cmake ];
+  src-libsimulator = fetchurl {
+    url = "https://github.com/arvidn/libsimulator/archive/aa6e074c10538f2a5ff1e8ff344e2959e08b9be7.tar.gz";
+    hash = "sha256-0qFQlwSjjifGpEIbUGtZEbfR6GfeI813RXpdAav/oHo=";
+  };
+
+  src-try_signal = fetchurl {
+    url = "https://github.com/arvidn/try_signal/archive/105cce59972f925a33aa6b1c3109e4cd3caf583d.tar.gz";
+    hash = "sha256-bxEbDXdCmoBRvk+u0GzyP7A89u4jOWfIT92djTtCuo4=";
+  };
+
+  src-asio-gnutls = fetchurl {
+    url = "https://github.com/paullouisageneau/boost-asio-gnutls/archive/a57d4d36923c5fafa9698e14be16b8bc2913700a.tar.gz";
+    hash = "sha256-hlohJW3giUW3nr3GYnDxCvuSimOOLBgp55SMWXbyidU=";
+  };
+
+  postUnpack = ''
+    pushd $sourceRoot
+
+    mkdir -p simulation/libsimulator
+    tar -C simulation/libsimulator -x -f ${src-libsimulator} --strip-components=1
+
+    mkdir -p deps/try_signal
+    tar -C deps/try_signal -x -f ${src-try_signal} --strip-components=1
+
+    mkdir -p deps/asio-gnutls
+    tar -C deps/asio-gnutls -x -f ${src-asio-gnutls} --strip-components=1
+
+    popd
+  '';
+
+  nativeBuildInputs = [
+    unzip
+    cmake
+  ];
 
   buildInputs = [
     boostPython

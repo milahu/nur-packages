e8fad9c6967ac8f35c3706d1a2ac1d6b75d5bfa5
Milan Hauth <milahu@gmail.com>
2025-06-11 15:00:43 +0200

    python3.pkgs.stream-zip: fix source

--- pkgs/python3/pkgs/stream-zip/stream-zip.nix
+++ pkgs/python3/pkgs/stream-zip/stream-zip.nix
@@ -1,6 +1,23 @@
+/*
+the source was removed from github
+https://web.archive.org/web/20240918163105/https://github.com/uktrade/stream-zip
+
+reconstructed source with git tags:
+https://github.com/milahu/stream-zip
+
+alternatives:
+https://github.com/sandes/zipfly # 2025, 500 stars
+https://github.com/arjan-s/python-zipstream # 2023, 50 stars
+https://github.com/kbbdy/zipstream # 2020, 40 stars
+https://github.com/pR0Ps/zipstream-ng # 30 stars
+https://github.com/DoctorJohn/aiohttp-zip-response # 1 stars
+https://github.com/baldhumanity/py_stream_zip # 0 stars
+*/
+
 { lib
 , python3
 , fetchFromGitHub
+, fetchPypi
 , fetchurl
 }:
 
@@ -12,8 +29,15 @@ python3.pkgs.buildPythonPackage rec {
   src =
   if true then
   fetchurl {
-    url = "https://github.com/uktrade/stream-zip/archive/refs/tags/v${version}.tar.gz";
-    hash = "sha256-LAlABuI/9lWtBr8uiH2Vkc8VYD6h0CwUXMGTwZgOvtI=";
+    url = "https://files.pythonhosted.org/packages/08/8a/0674a98c1d3d8edf1461b92474ed9cfe19298a297aab9a8ccd9ae1f15b07/stream_zip-0.0.83.tar.gz";
+    hash = "sha256-h9dV8M5D38awrlVbcz8WLHbi6vzzx4BdP1kelmsxZxY=";
+  }
+  else
+  if true then
+  # http 404 ?!
+  fetchPypi {
+    inherit pname version;
+    hash = "";
   }
   else
   fetchFromGitHub {
@@ -53,7 +77,7 @@ python3.pkgs.buildPythonPackage rec {
 
   meta = with lib; {
     description = "Python function to construct a ZIP archive on the fly";
-    homepage = "https://github.com/uktrade/stream-zip";
+    homepage = "https://pypi.org/project/stream-zip/";
     license = licenses.mit;
     maintainers = with maintainers; [ ];
   };

b7168784962ce0719d00d8a35fb26c74de82c5ee
Milan Hauth <milahu@gmail.com>
2025-07-01 10:49:40 +0200

    nginxModules.markdown_filter: up

--- pkgs/servers/http/nginx/modules/markdown_filter.nix
+++ pkgs/servers/http/nginx/modules/markdown_filter.nix
@@ -1,3 +1,7 @@
+# note: this requires a patched nginx/generic.nix
+# to use the module attributes configureFlags, src.patchPhase, buildInputs
+# see pkgs.nur.repos.milahu.nginx
+
 {
   lib,
   fetchFromGitHub,
@@ -7,42 +11,17 @@
 
 rec {
   name = "ngx_markdown_filter_module";
-  src-unpatched = fetchFromGitHub {
+  src = (fetchFromGitHub {
     owner = "ukarim";
     repo = "ngx_markdown_filter_module";
     # rev = "0.1.3";
     # https://github.com/ukarim/ngx_markdown_filter_module/pull/6
     rev = "11eef87a7966a72cbd10da1f80472b8b3a24c198";
     hash = "sha256-Ap22bM2HZ/F0WjkdQSPF4XcowxclE2b45jaL5MvNF1A=";
+  }) // {
+    patchPhase = "mv config_gfm config";
   };
-  src = stdenv.mkDerivation {
-    name = "source";
-    src = src-unpatched;
-    postPatch = ''
-      mv config_gfm config
-    '';
-    installPhase = ''
-      cp -r . $out
-    '';
-  };
-  # TODO make nginx/generic.nix use buildInputs
-  # buildInputs = [
-  # nixpkgs/pkgs/servers/http/nginx/generic.nix
-  # buildInputs = [ ... ] ++ mapModules "inputs"
-  # FIXME ngx_markdown_filter_module.c:10:14: fatal error: cmark.h: No such file or directory
-  inputs = [
-    cmark-gfm
-  ];
-  # TODO make nginx/generic.nix use postPatch
-  # no. postPatch is not used
-  /*
-  postPatch = ''
-    mv config_gfm config
-  '';
-  */
-  # configureFlags is not used by pkgs.nginx
-  # error: ngx_markdown_filter_module.c:10:14: fatal error: cmark.h: No such file or directory
-  # fix: use patched nginx in pkgs.nur.repos.milahu.nginx
+  buildInputs = [ cmark-gfm ];
   configureFlags = [ "--with-cc-opt=-DWITH_CMARK_GFM=1" ];
   meta = with lib; {
     description = "Markdown-to-html nginx module";

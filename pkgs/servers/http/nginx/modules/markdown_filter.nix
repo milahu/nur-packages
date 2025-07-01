a48f7071c413ac2c40a5e03a9c4efe7cf0ba490d
Milan Hauth <milahu@gmail.com>
2025-07-01 14:26:50 +0200

    nginxModules.markdown_filter: enable raw HTML (FIXME this is insecure)

--- pkgs/servers/http/nginx/modules/markdown_filter.nix
+++ pkgs/servers/http/nginx/modules/markdown_filter.nix
@@ -19,7 +19,22 @@ rec {
     rev = "11eef87a7966a72cbd10da1f80472b8b3a24c198";
     hash = "sha256-Ap22bM2HZ/F0WjkdQSPF4XcowxclE2b45jaL5MvNF1A=";
   }) // {
-    patchPhase = "mv config_gfm config";
+    patchPhase = ''
+      mv config_gfm config
+    ''
+    +
+    # fix: raw HTML omitted
+    # FIXME this is insecure
+    # https://github.com/commonmark/cmark#security
+    # we recommend you use a HTML sanitizer specific to your needs to protect against XSS attacks.
+    # FIXME enable the cmark-gfm tagfilter extension
+    # https://github.com/github/cmark-gfm/blob/master/extensions/tagfilter.c
+    # https://github.github.com/gfm/#disallowed-raw-html-extension-
+    # https://github.com/ukarim/ngx_markdown_filter_module/issues/7
+    # TODO? also remove dangerous links like <a href="javascript:alert(123)">clickme</a>
+    ''
+      sed -i 's/CMARK_OPT_DEFAULT/CMARK_OPT_DEFAULT | CMARK_OPT_UNSAFE/' ngx_markdown_filter_module.c
+    '';
   };
   buildInputs = [ cmark-gfm ];
   configureFlags = [ "--with-cc-opt=-DWITH_CMARK_GFM=1" ];

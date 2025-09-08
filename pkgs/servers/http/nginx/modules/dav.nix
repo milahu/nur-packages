a939500bb1e8a27cd99a615543fe38b9399d5993
Milan Hauth <milahu@gmail.com>
2025-09-08 13:25:24 +0200

    nginxModules.dav: 3.0.0 -> 9f112cf

--- pkgs/servers/http/nginx/modules/dav.nix
+++ pkgs/servers/http/nginx/modules/dav.nix
@@ -8,17 +8,17 @@
   name = "dav";
   src = fetchFromGitHub {
     name = "dav";
-    owner = "arut";
+    owner = "mid1221213";
     repo = "nginx-dav-ext-module";
-    rev = "v3.0.0";
-    sha256 = "000dm5zk0m1hm1iq60aff5r6y8xmqd7djrwhgnz9ig01xyhnjv9w";
+    rev = "9f112cf8e396ea5e1bdc70cedfa4f5cbc48fe98a";
+    hash = "sha256-BMYRH/BNuq/TTWPWdQJpz/Mx64vNEN7SQ/Swu3by92A=";
   };
 
   inputs = [ expat ];
 
   meta = with lib; {
     description = "WebDAV PROPFIND,OPTIONS,LOCK,UNLOCK support";
-    homepage = "https://github.com/arut/nginx-dav-ext-module";
+    homepage = "https://github.com/mid1221213/nginx-dav-ext-module";
     license = with licenses; [ bsd2 ];
     maintainers = [ ];
   };

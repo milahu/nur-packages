9fbc0c652f07e37ebc38e7168fc5365619592b30
Milan Hauth <milahu@gmail.com>
2025-10-05 09:50:28 +0200

    rqbit: 8.1.1 -> 9.0.0-beta.1-988d227

--- pkgs/by-name/rq/rqbit/package.nix
+++ pkgs/by-name/rq/rqbit/package.nix
@@ -9,16 +9,43 @@
   nodejs_20,
   nix-update-script,
 }:
+
+# librqbit-core@5.0.0 requires rustc 1.88
+let
+  # https://github.com/oxalica/rust-overlay/issues/209
+  rust-overlay-src = fetchFromGitHub {
+    owner = "oxalica";
+    repo = "rust-overlay";
+    rev = "971d18658c83f3a6a434ac647798141fddce3175";
+    hash = "sha256-LTgfljA/X8aGEXk/EUVoLL+0wfJjQAhF/rUQOFsx+/U=";
+  };
+  # re-evaluate pkgs
+  pkgsWithOverlay = import <nixpkgs> {
+    overlays = [ (import rust-overlay-src) ];
+  };
+in
+with pkgsWithOverlay;
+let
+  rustSpecific = rust-bin.stable.latest.default;
+  # rustSpecific = (rustChannelOf { channel = "1.90.0"; }).rust;
+  rustPlatform = makeRustPlatform {
+    cargo = rustSpecific;
+    rustc = rustSpecific;
+  };
+in
+
 let
   pname = "rqbit";
 
-  version = "8.1.1";
+  version = "9.0.0-beta.1-988d227";
 
   src = fetchFromGitHub {
     owner = "ikatson";
     repo = "rqbit";
-    rev = "v${version}";
-    hash = "sha256-5ErcI3hwC2EgxsjgEVlbHP1MzBf/LndpgTfynQGc29s=";
+    # rev = "v${version}";
+    # https://github.com/ikatson/rqbit/pull/489
+    rev = "988d2277d4a2a7101209c8fa8ada9de01139f6c8";
+    hash = "sha256-zHCYjOTm1qavleUF6DfNvi/Y0r/d6CI7p3SpVlRiyhQ=";
   };
 
   rqbit-webui = buildNpmPackage {
@@ -30,7 +57,7 @@ let
 
     sourceRoot = "${src.name}/crates/librqbit/webui";
 
-    npmDepsHash = "sha256-vib8jpf7Jn1qv0m/dWJ4TbisByczNbtEd8hIM5ll2Q8=";
+    npmDepsHash = "sha256-w75czvBzwVRNVXftVwJxWYTaCztXAKcfALc7Kve70fs=";
 
     installPhase = ''
       runHook preInstall
@@ -45,7 +72,7 @@ in
 rustPlatform.buildRustPackage {
   inherit pname version src;
 
-  cargoHash = "sha256-gYasOjrG0oeT/6Ben57MKAvBtgpoSmZ93RZQqSXAxIc=";
+  cargoHash = "sha256-OVVzWsF9xYlEDewF03JT9UBnc3XoHuNFyBF2gxO0Q74=";
 
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ pkg-config ];
 
